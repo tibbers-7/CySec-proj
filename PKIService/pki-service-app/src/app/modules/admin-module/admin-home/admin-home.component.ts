65adc4f8f4c37219ef2ff491b55c9b30cffeb3aa
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2023-04-13 00:35:47 +0200

    everything ready for be

--- PKIService/pki-service-app/src/app/modules/admin-module/admin-home/admin-home.component.ts
+++ PKIService/pki-service-app/src/app/modules/admin-module/admin-home/admin-home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Certificate } from 'src/app/model/certificate';
+import { CertificateService } from 'src/app/services/certificate.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -10,8 +11,17 @@ export class AdminHomeComponent {
 
   dataSource : Certificate[] = []
   displayedColumns = ['issuer','subject','type','status','revoke-button']
+  public constructor(private certificateService: CertificateService){}
+ 
+  onInit(){
+    this.certificateService.getAllCertificates().subscribe(res=>{
+      this.dataSource = res.data
+    })
+  }
 
-  revokeCertificate(certificate: any){
-
+  revokeCertificate(certificate: Certificate){
+  this.certificateService.revokeCertificate().subscribe(res=>{
+  console.log(res)
+})
   }
 }
