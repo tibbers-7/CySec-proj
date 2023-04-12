255a0adf990ce522e9dc1591eb5dab025f5bba3e
DanaBrasanac <brasanac.ra239.2018@uns.ac.rs>
2023-04-12 23:14:33 +0200

    changed port and css

--- PKIService/pki-service-app/src/app/services/certificate.service.ts
+++ PKIService/pki-service-app/src/app/services/certificate.service.ts
@@ -8,13 +8,12 @@ import { Observable } from "rxjs"
   })
   
   export class CertificateService {
-  //4200
-    apiHost: string = 'http://localhost:4200/api'
+    apiHost: string = 'http://localhost:8080/'
     headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
   
     constructor(private http: HttpClient) { }
   
     issueCertificate(certificateData: NewCertificateRequestData, keyStorePassword: string): Observable<any>{
-      return this.http.post<any>(this.apiHost + "/issueCertificate/" + keyStorePassword, certificateData ,{headers: this.headers})
+      return this.http.post<any>(this.apiHost + "issueCertificate/" + keyStorePassword, certificateData ,{headers: this.headers})
     }
   }
