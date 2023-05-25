7024e5598e9dcc5cbbbcd8c752c4f65f2663108e
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2023-05-25 21:27:42 +0200

    Create SecurityConfiguration.java

---
+++ PKIService/bezbednost-backend/src/main/java/com/example/bezbednostbackend/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.example.bezbednostbackend.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+//configures all http security of app
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                //whitelisting pages
+                .authorizeHttpRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                //decision management - if user authorized do not store session state
+                //ensures that each request is authenticated
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                //use filter before UsernamePasswordFilter
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
