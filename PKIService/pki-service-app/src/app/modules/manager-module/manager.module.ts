65c184b05d582d68e32d9366e145bcaed7ac6e97
IsidoraKnezevic <knezevic.ra47.2019@uns.ac.rs>
2023-06-18 00:06:13 +0200

    manager can view own projects

--- PKIService/pki-service-app/src/app/modules/manager-module/manager.module.ts
+++ PKIService/pki-service-app/src/app/modules/manager-module/manager.module.ts
@@ -6,16 +6,21 @@ import { FormsModule } from '@angular/forms';
 import { ManagerToolbarComponent } from './manager-toolbar/manager-toolbar.component';
 import { ManagerProfileComponent } from './manager-profile/manager-profile.component';
 import { RoleGuard } from 'src/app/auth/role-guard';
+import { ManagerProjectsComponent } from './manager-projects/manager-projects.component';
 
 const routes: Routes = [
   {path: 'manager/editProfile', component: ManagerProfileComponent,
+  canActivate: [RoleGuard], data: { expectedRole: 'ROLE_PROJECT_MANAGER' }},
+  {path: 'manager/projects', component: ManagerProjectsComponent,
   canActivate: [RoleGuard], data: { expectedRole: 'ROLE_PROJECT_MANAGER' }}
+
 ];
 
 @NgModule({
   declarations: [
     ManagerToolbarComponent,
-    ManagerProfileComponent
+    ManagerProfileComponent,
+    ManagerProjectsComponent
   ],
   imports: [
     AppRoutingModule,
